21dd5f682f931c8c5e1263bb57605359bbda96ed
koorosh <koorosh.akl@protonmail.com>
2024-10-19 20:14:13 +0300

    image gradient works, no optimizations yet.

--- main.scm
+++ main.scm
@@ -9,14 +9,36 @@ exec guile -L ./src -e main -s "$0" "$@"
 	     (ice-9 match))
 
 (define (float->int x) (inexact->exact (floor x)))
+(define (grad-pixel x d)
+  (inexact->exact (floor (* 255.999 (/ (* x 1.0) (1- d))))))
 
 (define (main args)
-  (define aspect-ratio (float->int 16/9))
   (define width 100)
+  (define file)
+
+  (match args
+    [(name) (display "bad args")]
+    [(name ofile) (set! file ofile)]
+    [(name ofile w) (set! file ofile) (set! width (string->number w))])
+
+  (define aspect-ratio 9/16)
   (define height (float->int (* width aspect-ratio)))
+
   (define vp-height 2.)
   (define vp-width (* vp-height (/ width height)))
-  (match args
-    [(name) #t]
-    [(name w) (set! width (string->number w))])
-  )
+
+  (define focal-length 1.0)
+
+  (define camera-center (vec3 0 0 0))
+
+  (define img (make-image height width))
+
+  (image-do img j i
+	    ;; (display (cons i j)) (newline)
+	    (color-set! img j i
+			(color (grad-pixel i width)
+			       (grad-pixel j height)
+			       (grad-pixel j height)
+			       0)))
+  (call-with-output-file file
+    (lambda (port) (write-image img port))))
