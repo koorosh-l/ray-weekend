864d105a077282a60f6b7ded888c1170d6fa2c5b
koorosh <koorosh.akl@protonmail.com>
2024-11-06 00:25:20 +0300

    vec optimizations and no main

--- main.scm
+++ main.scm
@@ -1,6 +1,6 @@
 #! /bin/sh
 # -*- mode: scheme; coding: utf-8 -*-
-exec guile -L ./src -e main -s "$0" "$@"
+exec guile -L ./src -s "$0" "$@"
 !#
 
 (use-modules (vec3)
@@ -21,44 +21,58 @@ exec guile -L ./src -e main -s "$0" "$@"
 	  (v3-add! c (vscale (vec3 .5 .7 1.) a) c)
 	  (vec3->color c)))))
 
-(define (main args)
-  (define width 100)
-  (define file)
+(define width 100)
+(define file)
 
-  (match args
-    [(name) (display "bad args")]
-    [(name ofile) (set! file ofile)]
-    [(name ofile w) (set! file ofile) (set! width (string->number w))])
+(match (command-line)
+  [(name) (display "bad args")]
+  [(name ofile) (set! file ofile)]
+  [(name ofile w) (set! file ofile) (set! width (string->number w))])
 
-  (define aspect-ratio 16/9)
-  (define height (float->int (/ width aspect-ratio)))
+(define aspect-ratio 16/9)
+(define height (float->int (/ width aspect-ratio)))
 
-  (define vp-height 2.)
-  (define vp-width (* vp-height (/ width height)))
+(define vp-height 2.)
+(define vp-width (* vp-height (/ width height)))
 
-  (define focal-length 1.0)
+(define focal-length 1.0)
 
-  (define camera-center (vec3 0 0 0))
+(define camera-center (vec3 0 0 0))
 
-  (define vp-u (vec3 vp-width 0 0))
-  (define vp-v (vec3 0 (- vp-height) 0))
+(define vp-u (vec3 vp-width 0 0))
+(define vp-v (vec3 0 (- vp-height) 0))
 
-  (define pixel-du (vscale vp-u (/ 1 width)))
-  (define pixel-dv (vscale vp-v (/ 1 height)))
-  (define vp-upper-left
-    (neg (v3-add (neg camera-center)
-		 (v3-add (vec3 0 0 focal-length)
-			 (v3-add (vscale vp-u .5)
-				 (vscale vp-v .5))))))
-  (define pixel00-loc (v3-add vp-upper-left
-			      (vscale (v3-add pixel-du pixel-dv) .5)))
+(define pixel-du (vscale vp-u (/ 1 width)))
+(define pixel-dv (vscale vp-v (/ 1 height)))
+(define vp-upper-left
+  (neg (v3-add (neg camera-center)
+	       (v3-add (vec3 0 0 focal-length)
+		       (v3-add (vscale vp-u .5)
+			       (vscale vp-v .5))))))
+(define pixel00-loc (v3-add vp-upper-left
+			    (vscale (v3-add pixel-du pixel-dv) .5)))
 
-  (define img (make-image height width))
-  (image-do img j i
-	    (let* ([pcenter (v3-add pixel00-loc (v3-add (vscale pixel-du i) (vscale pixel-dv j)))]
-		   [ray-dir (v3-add pcenter (neg camera-center))]
-		   [r (ray camera-center ray-dir)])
-	      (color-set! img j i
-			  (ray-color r))))
-  (call-with-output-file file
-    (lambda (port) (write-image img port))))
+(define img (make-image height width))
+(define z (vec3 0 0 0))
+(define pcenter (vec3 0 0 0))
+(define ray-dir (vec3 0 0 0))
+(define intm0 (vec3 0 0 0))
+(define intm1 (vec3 0 0 0))
+
+(image-do img j i
+	  (<- pcenter z)
+	  (<- intm0 pixel-du)
+	  (<- intm1 pixel-dv)
+	  (vscale! intm0 i)
+	  (vscale! intm1 j)
+	  (v3-add! intm0 intm0 intm1)
+	  (v3-add! pcenter pixel00-loc intm0)
+	  (lnr pcenter)
+	  (<- intm0 camera-center)
+	  (neg! intm0)
+	  (v3-add! ray-dir pcenter intm0)
+	  (color-set! img j i
+		      (ray-color (ray camera-center ray-dir))))
+
+(call-with-output-file file
+  (lambda (port) (write-image img port)))
