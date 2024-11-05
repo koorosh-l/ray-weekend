1178925a09a78997b650e829b2b7109915d15f9c
koorosh <koorosh.akl@protonmail.com>
2024-11-05 19:00:18 +0300

    sphere collision

--- src/ray.scm
+++ src/ray.scm
@@ -1,5 +1,6 @@
 (define-module (ray)
-  #:use-module (vec3))
+  #:use-module (vec3)
+  #:export (ray-at sphere-colide))
 
 (define-public ray cons)
 (define-public dir cdr)
@@ -9,6 +10,15 @@
   (define ret (vscale (dir ray) t))
   (v3-add! ret (origin ray) ret)
   ret)
-
-(define (sphere-colide ray sph-origin sph-diameter) ;;return t
-  1)
+;; returns a cons cell or empty list if there is no intersection
+(define (sphere-colide ray sph-origin sph-radius)
+  (let* ([q (origin ray)]
+	 [d (dir    ray)]
+	 [c sph-origin]
+	 [c-q (v3-add c (neg q))]
+	 [r sph-radius]
+	 [a (dot d d)]
+	 [b (dot (vscale d -2) c-q)]
+	 [c (- (dot c-q c-q) (* r r))]
+	 [disc (- (* b b) (* 4 a c))])
+    (>= disc 0)))
