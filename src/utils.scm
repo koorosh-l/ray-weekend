43f6c0fccf517ff1df13eb115bfa4ae39bb4b557
koorosh <koorosh.akl@protonmail.com>
2024-11-10 09:35:25 +0300

    for benchmarking

--- src/utils.scm
+++ src/utils.scm
@@ -1,7 +1,15 @@
 (define-module (utils)
   #:export (lnr float->int))
+
 (define-inlinable (float->int x) (inexact->exact (floor x)))
+
 (define-syntax-rule (lnr exp ...)
   (let ([e (begin exp ...)])
     (format (current-error-port) "----------~a ~a\n" (current-source-location) e)
     e))
+
+(define (do-time thunk n)
+  (let loop ([n n])
+    (cond
+     [(zero? n) #t]
+     [else (thunk) (loop (1- n))])))
